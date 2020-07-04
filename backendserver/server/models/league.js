1371f77de86a6b8252fa232f4008e6a3cffe2df9
sir-onze <A75328@alunos.uminho.pt>
2020-07-04 16:09:14 +0100

    games

--- backendserver/server/models/league.js
+++ backendserver/server/models/league.js
@@ -5,7 +5,8 @@ var leagueSchema = new mongoose.Schema({
     name: {type:String, required: true},
     teams: [Array],
     round: {type:Number, required:true},
-
+    games: [Array],
+    state : {type:Number, required:true}, // open or closed for inscription
 })
 
 module.exports = mongoose.model('league', leagueSchema)
