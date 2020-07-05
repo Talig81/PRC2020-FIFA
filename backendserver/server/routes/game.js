0b0fce09f80ae16276018a667b9431e1b33028ae
sir-onze <A75328@alunos.uminho.pt>
2020-07-05 11:57:07 +0100

    backend completo

---
+++ backendserver/server/routes/game.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var GameController = require('../controllers/game')
+var Game= require('../models/game')
+var passport = require('passport')
+
+/* GET de todos os jogos */
+
+router.get('/list/:id', function(req, res, next) {
+  GameController.getGame
+    .then(dados => res.jsonp(dados))
+    .catch(e => res.status(500).send(`Erro na listagem de jogos: ${e}`))
+});
+
+/** A adição de um jogo está a ser feita do lado da liga */
+
+// lista de jogos de uma liga
+router.get('/list/:name', function(req, res, next) {
+  
+      GameController.getGameOfLeague(req.params.name)
+        .then(dados => res.jsonp(dados))
+        .catch(e => res.status(500).send(`Erro na listagem de jogos: ${e}`))
+    });
+
+    
+module.exports = router;
